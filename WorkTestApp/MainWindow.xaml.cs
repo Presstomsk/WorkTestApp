a086ae38697ce3ca63ad548d68a14544500116fa
Roman Permiakov <info@presstomsk.ru>
2021-12-07 00:24:40 +0700

    Testing
    Test2
    
    Test3
    
    Testing
    
    Ok

--- WorkTestApp/MainWindow.xaml.cs
+++ WorkTestApp/MainWindow.xaml.cs
@@ -18,10 +18,42 @@ namespace WorkTestApp
   
     public partial class MainWindow : Window
     {
+        /* LinkedList<int> one; 
+
+         LinkedList<int> two; 
+         LinkedList<int> result; */
+        /*List<int> nev = new List<int>() {4,4,3,6,7,3,8,8,4,8,9,5,6,7 };*/
         public MainWindow()
         {
             InitializeComponent();
-            MessageBox.Show($"{ThirdApp.IsPalindrome("А роза упала, на лапу Азора")}");
+
+            //MessageBox.Show($"{ThirdApp.IsPalindrome("А роза упала, на лапу Азора")}");
+            /*one = new LinkedList<int>();
+            two= new LinkedList<int>();
+            result = new LinkedList<int>();
+            one.AddLast(5);
+            one.AddLast(8);
+            one.AddLast(4);
+            one.AddLast(7);
+            two.AddLast(4);
+            two.AddLast(6);
+            two.AddLast(5);
+
+            result = SecondApp.SumTwoLists(one, two);
+            var item = result.First;
+            string str=String.Empty;
+            while (item != null)
+            {
+                str += item.Value.ToString();
+                item = item.Next;
+            }
+            New.Text = str;*/
+
+            //New.Text=FirstApp.SumMod(nev).ToString();
+
+
         }
+
+
     }
 }
